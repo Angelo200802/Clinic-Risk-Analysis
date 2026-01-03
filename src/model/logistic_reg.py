e69fbd6e06c64f027d08004dfd5aac648cbaf868
Angelo200802 <angelo.capogrosso02@gmail.com>
2026-01-03 20:32:51 +0100

    Enhance logistic regression and random forest models: add PolynomialExpansion for feature transformation, update feature selection, and improve pipeline structure; add correlation analysis function in random forest model; create new SVM model file.

--- src/model/logistic_reg.py
+++ src/model/logistic_reg.py
@@ -1,7 +1,7 @@
 from pyspark.ml.feature import StandardScaler
 from src.spark_manager import get_session, load_dataset
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
-from pyspark.ml.feature import VectorAssembler, StringIndexer
+from pyspark.ml.feature import VectorAssembler, StringIndexer, PolynomialExpansion
 from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml import Pipeline, PipelineModel
@@ -14,6 +14,12 @@ SAVE_MODEL_PATH = os.getenv("SAVE_MODEL_PATH")
 
 spark = get_session()   
 ds = load_dataset(spark, DS_PATH)
+
+polyExpansion = PolynomialExpansion(
+    degree=2, 
+    inputCol="features", 
+    outputCol="polyFeatures"
+)
 
 indexer_gender = StringIndexer(
     inputCol = "Gender",
@@ -28,24 +34,29 @@ indexer_risk = StringIndexer(
 )
 
 assembler = VectorAssembler(
-    inputCols=[col for col in ds.columns if col not in  [ds.columns[-1],"Gender", "Timestamp", "Patient ID", "Height", "Weight", "Systolic Blood Pressure", "Diastolic Blood Pressure"]],
+    inputCols=[col for col in ds.columns if col not in  [ds.columns[-1],"Gender", "Gender_b", "Timestamp", "Patient ID", "Weight (kg)", "Height (m)", "Systolic Blood Pressure", "Diastolic Blood Pressure"]],
     outputCol="features_unscaled"
+)
+
+assembler2 = VectorAssembler(
+    inputCols=[col for col in ds.columns if col not in  [ds.columns[-1],"Gender", "Timestamp", "Patient ID", "Weight (kg)", "Height (m)", "Systolic Blood Pressure", "Diastolic Blood Pressure"]],
+    outputCol="features"
 )
 
 scaler = StandardScaler(
     inputCol="features_unscaled", 
-    outputCol="features", 
+    outputCol="features1", 
     withStd=True, 
     withMean=True
 )
 
-lr = LogisticRegression(labelCol="RiskCategory_b", featuresCol="features")
+lr = LogisticRegression(labelCol="RiskCategory_b", featuresCol="polyFeatures")
 
-pipe = Pipeline(stages=[indexer_gender, indexer_risk, assembler,scaler, lr])
+pipe = Pipeline(stages=[indexer_gender, indexer_risk, assembler,scaler,assembler2,polyExpansion, lr])
 
 paramGrid = (ParamGridBuilder()
              .addGrid(lr.regParam, [0.0001,0.001,0.01,0.1,1])#[i for i in np.arange(0,0.11,0.01)])
-             .addGrid(lr.elasticNetParam, [0.7,0.8,0.9])#[i for i in np.arange(0,1.1,0.1)]) 
+             .addGrid(lr.elasticNetParam, [0.0,0.5,0.9])#[i for i in np.arange(0,1.1,0.1)]) 
              .addGrid(lr.maxIter, [10,100,1000])#[i for i in range(0,100,10)])               
              .build())
 
@@ -58,22 +69,39 @@ cv = CrossValidator(
     numFolds=5                    # Divide i dati in 5 parti (5-fold cross validation)
 )
 
+def get_poly_feature_names(base_features, degree=2):
+    """
+    Ricostruisce i nomi delle feature generati da PolynomialExpansion (grado 2)
+    L'ordine di Spark è: feature originali, poi le interazioni/quadrati
+    """
+    poly_names = []
+    # 1. Feature originali
+    poly_names.extend(base_features)
+    
+    # 2. Interazioni e quadrati (per grado 2)
+    if degree >= 2:
+        for i in range(len(base_features)):
+            for j in range(i, len(base_features)):
+                poly_names.append(f"{base_features[i]} * {base_features[j]}")
+    
+    return poly_names
+
 def evaluate_model(predictions,label):
     evaluator = MulticlassClassificationEvaluator(labelCol=label, predictionCol="prediction")
     accuracy = evaluator.setMetricName("accuracy").evaluate(predictions)
     precision = evaluator.setMetricName("weightedPrecision").evaluate(predictions)
     recall = evaluator.setMetricName("weightedRecall").evaluate(predictions)
     f1 = evaluator.setMetricName("f1").evaluate(predictions)
+    evaluator = BinaryClassificationEvaluator(labelCol="RiskCategory_b",metricName="areaUnderROC")
     print(f"Accuracy: {accuracy}")
     print(f"Precision: {precision}")
     print(f"Recall: {recall}")
     print(f"F1 Score: {f1}")
+    print(f"AUC ROC: {evaluator.evaluate(predictions)}")
 
 def fit(cv:CrossValidator,train,save:bool = True,path = ""):
     model = cv.fit(train) 
     best_model = model.bestModel
-    clf_model = best_model.stages[-1]
-    #print(f"Summary:\n {clf_model.explainParams()}")
     if save:
         try:
             best_model.save(f"{path}_pipeline")
@@ -84,15 +112,28 @@ def fit(cv:CrossValidator,train,save:bool = True,path = ""):
 
 if __name__ == "__main__":
     train, test = ds.randomSplit([0.7, 0.3], seed=42)
-    model = fit(cv,train,path=".src/model/saved_models/log_reg")
-    #model = PipelineModel.load("./src/model/saved_models/log_reg_pipeline")
+    #model = fit(cv,train,path="./src/model/saved_models/log_reg")
+    model = PipelineModel.load("./src/model/saved_models/log_reg_pipeline")
+    
     predictions = model.transform(test)
     predictions.groupBy("RiskCategory_b", "prediction").count().show()
     evaluate_model(predictions,"RiskCategory_b")
-    feature_names = [col for col in ds.columns if col not in ["RiskCategory_b", "Gender", "Timestamp", "Patient ID"]]
+    feature_names = [col for col in ds.columns if col not in [ds.columns[-1],"Gender", "Timestamp", "Patient ID", "Height (m)", "Weight (kg)", "Systolic Blood Pressure", "Diastolic Blood Pressure"]]
     coefficients = model.stages[-1].coefficients.toArray()
-    for name, coef in zip(feature_names, coefficients):
-        print(f"{name:25} : {coef:.4f}")
-    print("Regularization Parameter:", model.stages[-1].regParam)
-    print("ElasticNet Parameter:", model.stages[-1].elasticNetParam)
-    print("Max Iterations:", model.stages[-1].maxIter)
+    print(f"Intercetta: {model.stages[-1].intercept:.4f}")
+    print("\n--- Coefficients ---")
+    # Genera i nomi mappati
+    mapped_names = get_poly_feature_names(feature_names, degree=2)
+    coeff_list = []
+    for name, coeff in zip(mapped_names, coefficients):
+        coeff_list.append((name, coeff))
+    sorted_coeffs = sorted(coeff_list, key=lambda x: abs(x[1]), reverse=True)
+
+    print(f"{'Feature':<40} | {'Coefficiente':<12}")
+    print("-" * 55)
+    for name, val in sorted_coeffs[:15]: # Vediamo i primi 15
+        print(f"{name:<40} | {val:>12.4f}")
+    print("Regularization Parameter:", model.stages[-1].getOrDefault('regParam'))
+    print("ElasticNet Parameter:", model.stages[-1].getOrDefault('elasticNetParam'))
+    print("Max Iterations:", model.stages[-1].getOrDefault('maxIter'))
+    
--- src/model/rnd_forest.py
+++ src/model/rnd_forest.py
@@ -1,11 +1,12 @@
 from src.spark_manager import get_session, load_dataset
-from src.model.logistic_reg import indexer_gender, indexer_risk, assembler, evaluate_model, fit
+from src.model.logistic_reg import indexer_gender, scaler,indexer_risk, evaluate_model, fit
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml import Pipeline, PipelineModel
+from pyspark.ml.feature import Bucketizer, VectorAssembler
 from dotenv import load_dotenv
-import os
+import os, matplotlib.pyplot as plt, seaborn as sns, pandas as pd
 
 load_dotenv()
 DS_PATH = os.getenv("DATASET_PATH")
@@ -14,13 +15,36 @@ SAVE_MODEL_PATH = os.getenv("SAVE_MODEL_PATH")
 spark = get_session()   
 ds = load_dataset(spark, DS_PATH)
 
-rf_clf = RandomForestClassifier(labelCol="RiskCategory_b", featuresCol="features", seed=42)
+bmi_buckets = [-float("inf"), 18.5, 25.0, 30.0, float("inf")]   
+bmi_bucketizer = Bucketizer(
+    splits=bmi_buckets,
+    inputCol="Derived_BMI",
+    outputCol="BMI_binned"
+)
 
-pipe = Pipeline(stages=[indexer_gender, indexer_risk, assembler,rf_clf])
+features_to_use = [
+    "Heart Rate", "Respiratory Rate", "Body Temperature", 
+    "Oxygen Saturation", "Age", "Derived_HRV", 
+    "Derived_Pulse_Pressure", "Derived_MAP", 
+    "Gender_b"
+]
+
+assembler = VectorAssembler(
+    inputCols=features_to_use,
+    outputCol="features_unscaled"
+)
+
+rf_clf = RandomForestClassifier(
+    labelCol="RiskCategory_b", 
+    maxMemoryInMB=1024, 
+    featuresCol="features", 
+    seed=42)
+
+pipe = Pipeline(stages=[indexer_gender, indexer_risk, assembler,scaler,rf_clf])
 
 param_grifd = ( ParamGridBuilder()
              .addGrid(rf_clf.numTrees, [10,50,100])
-             .addGrid(rf_clf.maxDepth, [10,15,20])
+             .addGrid(rf_clf.maxDepth, [5,10,15])
              .addGrid(rf_clf.impurity, ["gini", "entropy"])
              .build() 
              )
@@ -31,14 +55,27 @@ cv = CrossValidator(
     estimatorParamMaps=param_grifd,
     evaluator=evaluator,
     seed=42,
-    parallelism=2,
-    numFolds=10                    
+    parallelism=3,
+    numFolds=5                    
 )
+
+def analyze_correlation(df:pd.DataFrame):
+    # Calcolo della matrice di correlazione (metodo di Pearson)
+    corr_matrix = df.corr()
+
+    # Visualizzazione con Heatmap
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+    plt.title('Matrice di Correlazione delle Feature')
+    plt.show()
 
 if __name__ == "__main__":
     train, test = ds.randomSplit([0.7, 0.3], seed=42)
-    #fit(cv,train,test,"RiskCategory_b","Random Forest Classifier")
-    model = PipelineModel.load("./src/model/saved_models/random_forest_pipeline_model")
+    #ds_pd = ds.toPandas().drop(columns=["Timestamp","Patient ID"])
+    #ds_pd = pd.get_dummies(ds_pd, columns=["Gender","Risk Category"],dtype=int,drop_first=True)
+    #analyze_correlation(ds_pd)
+    model = fit(cv,train,test,path="./src/model/saved_models/rnd_forest")
+    #model = PipelineModel.load("./src/model/saved_models/random_forest_pipeline_model")
     importances = model.stages[-1].featureImportances
     # Se l'ID è nella lista, vedrai qualcosa del genere:
     for i, column in enumerate(assembler.getInputCols()):
